dd415970b90b36f5e8efe3d3150e67ab546a2f28
JehtBlack <jehtblack7@gmail.com>
2019-10-04 23:56:29 +0100

    Game Manager Instance class

---
+++ Assets/GameManagerInstance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerInstance : MonoBehaviour {
+
+    // helper defines
+    public enum UnlockStateID : UInt16 {
+
+    }
+
+    // data
+    public static GameManagerInstance Instance;
+
+    [NonSerialized]
+    private Dictionary<UnlockStateID, UInt64> UnlockCosts = new Dictionary<UnlockStateID, UInt64> {
+
+    };
+
+    private UInt64 Currency = 0;
+    private List<bool> UnlockStates = new List<bool>();
+
+    // methods
+    public bool GetState(UnlockStateID id) {
+        UInt16 idx = (UInt16)id;
+        if (idx > UnlockStates.Count) {
+            while (UnlockStates.Count <= idx)
+                UnlockStates.Add(false);
+        }
+        return UnlockStates[(int)idx];
+    }
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
