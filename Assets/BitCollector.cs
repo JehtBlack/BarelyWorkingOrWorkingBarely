e81c321945bfc892e8358e84f1a7c456f71cb575
cfharty <cfharty@gmail.com>
2019-10-05 18:09:24 +0100

    Bit sprite animation, some simple collision interfacing

---
+++ Assets/BitCollector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitCollector : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            collision.gameObject.GetComponent<ICollectible>().Collected();
+        }
+    }
+}
---
+++ Assets/CollectableBit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableBit : MonoBehaviour, ICollectible
+{
+    [SerializeField]
+    private ulong CurrencyValue = 0;
+
+    public void Collected()
+    {
+        GameManagerInstance.Instance.AddCurrency(CurrencyValue);
+
+        Destroy(gameObject);
+    }
+}
