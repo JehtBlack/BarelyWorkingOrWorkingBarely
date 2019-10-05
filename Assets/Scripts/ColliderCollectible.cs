31fa682bdfa4ecd7d6d17a941a3cb8094a625fac
JehtBlack <jehtblack7@gmail.com>
2019-10-05 18:53:09 +0100

    added script for colliding type collections to make use of

---
+++ Assets/Scripts/ColliderCollectible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CurrencyAcquisition), typeof(Collider2D))]
+public class ColliderCollectible : MonoBehaviour {
+    private CurrencyAcquisition acquisitionTrigger;
+    void Awake() {
+        acquisitionTrigger = gameObject.GetComponent<CurrencyAcquisition>();
+    }
+    void OnCollisionEnter2D(Collision2D col) {
+        acquisitionTrigger.TriggerCollection();
+    }
+}
