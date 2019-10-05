f25027a497a10eeb8ab2eee0e2c9d7ca384d62fb
cfharty <cfharty@gmail.com>
2019-10-05 16:48:31 +0100

    First player controller scripts, some crap ground assets

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject Target = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Target != null)
+            gameObject.transform.position = Target.transform.position;    
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller2D;
+    [Range(0, 100.0f)] public float MovementSpeed = 10.0f;
+
+
+    private bool Jump = false;
+    private float HorizontalMove = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * MovementSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller2D.Move((HorizontalMove * Time.fixedDeltaTime), false, Jump);
+        Jump = false;
+    }
+}
