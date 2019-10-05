e3d206fe19b3d8124587aac567a0abfdcbe8bde4
JehtBlack <jehtblack7@gmail.com>
2019-10-05 20:03:18 +0100

    there greyscale can happen now

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera), typeof(UnityEngine.PostProcessing.PostProcessingBehaviour))]
+public class CameraController : MonoBehaviour {
+    [SerializeField] 
+    private UnityEngine.PostProcessing.PostProcessingProfile GreyScaleProfile;
+
+    // Start is called before the first frame update
+    void Start() {
+        GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour>().profile = GreyScaleProfile;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
