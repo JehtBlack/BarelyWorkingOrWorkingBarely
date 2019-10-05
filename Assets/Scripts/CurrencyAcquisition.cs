62be1fd8c13e5d532408f3d1652457fedd96b133
JehtBlack <jehtblack7@gmail.com>
2019-10-05 18:09:26 +0100

    added currency acquisition monobehaviour with value falloff based on collection count

---
+++ Assets/Scripts/CurrencyAcquisition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyAcquisition : MonoBehaviour {
+    [SerializeField] 
+    private UInt64 Value = 0;
+    
+    [SerializeField] 
+    private AnimationCurve ValueFalloff = new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 0));
+    
+    [SerializeField] 
+    [Range(1, 100)]
+    private int MaxCollectionCount = 1;
+
+    private int CollectionCount = 0;
+    
+    void TriggerCollection() {
+
+        float t = CollectionCount / (float) MaxCollectionCount;
+        float evaluation = Mathf.Lerp(0, MaxCollectionCount, t);
+
+        float valuation = ValueFalloff.Evaluate(evaluation);
+        UInt64 currencyValue = (UInt64)Mathf.Abs(Mathf.FloorToInt(Value * valuation));
+        GameManagerInstance.Instance.AddCurrency(currencyValue);
+        CollectionCount++;
+    }
+}
