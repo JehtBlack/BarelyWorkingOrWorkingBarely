800574b854135c6859875dc508d16ba920fa5312
cfharty <cfharty@gmail.com>
2019-10-06 15:40:05 +0100

    Added shop sign prefab, tile grid background, tilesets for floor and objects, a few boxes, updated the bit prefab, updated shop controller.

--- Assets/ShopController.cs
+++ Assets/ShopController.cs
@@ -4,25 +4,48 @@ using UnityEngine;
 
 [RequireComponent(typeof(Canvas))]
 public class ShopController : MonoBehaviour {
-    
+
+    public static ShopController Instance;
+
+    private Canvas ShopCanvas;
+
     public void Awake() {
-        Canvas canvas = GetComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceCamera;
-        canvas.worldCamera = Camera.main;
+        ShopCanvas = GetComponent<Canvas>();
+        ShopCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+        ShopCanvas.worldCamera = Camera.main;
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
-
-
-
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Shop"))
-            gameObject.GetComponent<Canvas>().enabled = !gameObject.GetComponent<Canvas>().enabled;
+        //if (Input.GetButtonDown("Shop"))
+        //    gameObject.GetComponent<Canvas>().enabled = !gameObject.GetComponent<Canvas>().enabled;
+    }
+
+    public void OpenShop()
+    {
+          ShopCanvas.enabled = true;
+    }
+    public void CloseShop()
+    {
+        ShopCanvas.enabled = true;
+    }
+    public void ToggleShop()
+    {
+        ShopCanvas.enabled = !ShopCanvas.enabled;
     }
 }
---
+++ Assets/ShopSign.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSign : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Shop") && CanOpenShop)
+            ShopController.Instance.ToggleShop();
+
+    }
+
+
+
+    private bool CanOpenShop = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            CanOpenShop = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            CanOpenShop = false;
+    }
+}
