fb6b991b714eeb0aba4b31c6e32a9b0063ac8f12
JehtBlack <jehtblack7@gmail.com>
2019-10-06 23:48:30 +0100

    fixed up settings menu to toggle things that aren't a straight unlock Colour is the first it now listens to the setting being changed rather than the unlock

--- Assets/Scripts/SettingController.cs
+++ Assets/Scripts/SettingController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,16 @@ public class SettingController : MonoBehaviour
     public static SettingController Instance;
 
     private Canvas SettingCanvas;
+
+    [NonSerialized]
+    public readonly List<GameManagerInstance.UnlockStateID> SettingsFilter = new List<GameManagerInstance.UnlockStateID> {
+        GameManagerInstance.UnlockStateID.ColourVision
+    };
+
+    [NonSerialized] 
+    public readonly Dictionary<GameManagerInstance.UnlockStateID, bool> Settables = new Dictionary<GameManagerInstance.UnlockStateID, bool>();
+
+    public event Action<GameManagerInstance.UnlockStateID, bool /*oldState*/, bool /*newState*/> OnSettingChanged;
 
     public void Awake()
     {
@@ -23,6 +34,33 @@ public class SettingController : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        GameManagerInstance.Instance.UnlockStateChanged += OnUnlock;
+    }
+
+    public void SetSettable(GameManagerInstance.UnlockStateID id, bool state) {
+        // if id must alter other settables check here and turn off other settings
+
+        bool oldState = false;
+        if (Settables.ContainsKey(id))
+            oldState = Settables[id];
+
+        Settables[id] = state;
+        OnSettingChanged?.Invoke(id, oldState, Settables[id]);
+    }
+
+    public bool? GetSettable(GameManagerInstance.UnlockStateID id) {
+        if (!Settables.ContainsKey(id))
+            return null;
+        return Settables[id];
+    }
+
+    void OnUnlock(GameManagerInstance.UnlockStateID id, bool oldState, bool newState) {
+        if (SettingsFilter.Contains(id)) {
+            SetSettable(id, newState);
         }
     }
 
