6b4a192e447dd5b03602a4f7fc8525cb6c4c7be8
JehtBlack <jehtblack7@gmail.com>
2019-10-06 21:11:41 +0100

    Swapping tilesets

---
+++ Assets/Scripts/TileMapControllerEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileMapController))]
+public class TileMapControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        TileMapController controller = (TileMapController)target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Toggle Tileset"))
+            controller.SwapTileset();
+    }
+}
