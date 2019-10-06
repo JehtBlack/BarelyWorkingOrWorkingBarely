e3bddc726c237699a96f1404aafa35d665c59ebd
JehtBlack <jehtblack7@gmail.com>
2019-10-06 21:11:41 +0100

    Swapping tilesets

---
+++ Assets/Scripts/ScriptableObjects/BasicTile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Basic Tile", menuName = "Tiles/Basic Tile")]
+public class BasicTile : Tile {
+
+}
---
+++ Assets/Scripts/TileMapController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class TileMapController : MonoBehaviour {
+
+    public List<TileBase> PrimaryTileSet;
+    public List<TileBase> SecondaryTileSet;
+
+    private Tilemap Map;
+    private bool PrimaryIsActive = true;
+
+    void Awake() {
+        Map = GetComponent<Tilemap>();
+    }
+
+    public void SwapTileset() {
+        int tileCount = Mathf.Min(PrimaryTileSet.Count, SecondaryTileSet.Count);
+        List<TileBase> from;
+        List<TileBase> to;
+        if (PrimaryIsActive) {
+            from = PrimaryTileSet;
+            to = SecondaryTileSet;
+        }
+        else {
+            from = SecondaryTileSet;
+            to = PrimaryTileSet;
+        }
+
+        for (int i = 0; i < tileCount; ++i) {
+            Map.SwapTile(from[i], to[i]);
+        }
+
+        PrimaryIsActive = !PrimaryIsActive;
+    }
+}
