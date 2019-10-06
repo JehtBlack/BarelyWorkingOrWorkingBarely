97bf9df558ebda88c286084ae8e4ae5bd17c8056
JehtBlack <jehtblack7@gmail.com>
2019-10-06 22:44:21 +0100

    remove unused script

--- Assets/Scripts/ScriptableObjects/BasicTile.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-[CreateAssetMenu(fileName = "New Basic Tile", menuName = "Tiles/Basic Tile")]
-public class BasicTile : Tile {
-
-}
